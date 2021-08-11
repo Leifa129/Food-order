69cab202c4fd75fe74c4982025755ca7e4965528
Leifa129 <leif_xx@hotmail.com>
2021-08-11 12:24:09 +0200

    Food order project

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react';
+import classes from  './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
+import CartContext from "../../store/cart-context";
+
+function HeaderCartButton(props) {
+    const cartCtx = useContext(CartContext);
+    // const {items} = cartCtx;
+   const numberOfCartItems = cartCtx.items.reduce((acc, item) => {return item.amount + acc}, 0 );
+
+
+
+
+    return (
+        <button className={classes.button} onClick={props.onClick}>
+            <span className={classes.icon}><CartIcon /></span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
+        </button>
+    );
+}
+
+export default HeaderCartButton;
